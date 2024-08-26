14be73b55456d5beddac856ed66072ed22774157
Ridham Patel <ridham.patel@adp.com>
2024-08-26 16:21:25 -0400

    added pages and also card which can be used at all around

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import NavBar from './components/NavBar';
-import Dashboard from './components/Dashboard'
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import PostJob from './components/Job/PostJob'
+import Dashboard from './components/Dashboard';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import PostJob from './components/HR/PostJob';
+import Jobs from './components/Applicant/Jobs';
+import JobsApplied from './components/Applicant/JobsApplied';
+import Accepted from './components/HR/Accepted';
+import JobsHiring from './components/HR/JobsHiring';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -20,6 +24,10 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/jobshiring" element={<JobsHiring />} />
+            <Route path="/accepted" element={<Accepted />} />
+            <Route path="/jobsapplied" element={<JobsApplied />} />
+            <Route path="/jobs" element={<Jobs />} />
             <Route path="/Postjob" element={<PostJob />} />
           </Routes>
         </BrowserRouter>
---
+++ react/src/components/HR/JobsHiring.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useContext } from "react";
+
+
+const JobsHiring = () => {
+
+    return (
+        <>
+        <h1>Jobs added by HR Manager</h1>
+        </>
+    );
+};
+
+export default JobsHiring;
