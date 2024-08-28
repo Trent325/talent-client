b11a4d6479e8ab3c7ce44c989bd0b2e176b6ddf1
Ridham Patel <ridham.patel@adp.com>
2024-08-28 01:36:47 -0400

    added appliedjoblist page and jobdetail page

--- fix/ADPproject/src/components/router/Router.jsx
+++ fix/ADPproject/src/components/router/Router.jsx
@@ -6,6 +6,9 @@ import Login from "../auth/Login";
 import Register from "../auth/Register";
 import JobListpage from "../pages/JobListApplicant";
 import AuthRoute from "../context/route";
+import AppliedJobListApplicant from "../pages/AppliedJobListApplicant";
+import JobDetail from "../pages/JobDetail";
+
 
 const RouterApp = () => {
   return (
@@ -16,6 +19,8 @@ const RouterApp = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/jobList" element={<AuthRoute element={JobListpage} />} />
+        <Route path="/appliedjoblist" element={<AuthRoute element={AppliedJobListApplicant} />}/>
+        <Route path="/jobdetails/:id" element={<AuthRoute element={JobDetail}/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
